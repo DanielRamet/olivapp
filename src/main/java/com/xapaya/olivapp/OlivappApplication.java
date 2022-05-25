630073a0546c563badaebccf0a61432cf3d3129f
Xapaya <drametbarea@gmail.com>
2022-05-26 00:56:06 +0200

    Initial commit

---
+++ src/main/java/com/xapaya/olivapp/OlivappApplication.java
@@ -0,0 +1,13 @@
+package com.xapaya.olivapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlivappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlivappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xapaya/olivapp/OlivappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xapaya.olivapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlivappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
